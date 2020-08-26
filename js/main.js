dfa6275d8ca09bdae149aee57c07cdecbeba73d9
Alex <anonim01123@mail.ru>
2020-08-26 15:12:15 +0300

    Fourth commit: added favicon, some css animations and media queries

--- js/main.js
+++ js/main.js
@@ -31,14 +31,14 @@ document.addEventListener('DOMContentLoaded', () => {
         header = document.querySelector(".header"),
         sliderInterval = setInterval(styleChanger, 5000),
         counter = 1;
-        intros = {
-            purple: 'url("/images/bgr/intro.jpg") 0 0 / cover no-repeat',
-            green: 'linear-gradient(120deg, #000 45%, #51ff00)',
-            yellow: 'linear-gradient(120deg, #000 45%, #f7e600)',
-            red: 'linear-gradient(120deg, #000 45%, #ff0000)'
-        }
+    intros = {
+        purple: 'url("./images/bgr/intro.jpg") 0 0 / cover no-repeat',
+        green: 'linear-gradient(120deg, #000 45%, #51ff00)',
+        yellow: 'linear-gradient(120deg, #000 45%, #f7e600)',
+        red: 'linear-gradient(120deg, #000 45%, #ff0000)'
+    }
 
-    function styleChanger(){
+    function styleChanger() {
         dots.forEach((item) => {
             item.classList.remove("header__dot_active");
         });
@@ -46,10 +46,10 @@ document.addEventListener('DOMContentLoaded', () => {
         if (counter < 3) {
             header.style.background = Object.values(intros)[counter];
         } else {
-            header.style.background = Object.values(intros)[counter]; 
+            header.style.background = Object.values(intros)[counter];
             counter = -1;
         }
-        counter++;   
+        counter++;
     }
 
     dots.forEach(dot => {
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
             dots.forEach((item, i) => {
                 item.classList.remove("header__dot_active");
                 if (target == item) {
-                    header.style.background = Object.values(intros)[i];  
+                    header.style.background = Object.values(intros)[i];
                 }
             });
             dot.classList.add("header__dot_active");
@@ -72,29 +72,31 @@ document.addEventListener('DOMContentLoaded', () => {
     for (let i = 0; i < 12; i++) {
         let work = document.createElement("div");
         work.classList.add("works__item");
-        let color = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+        let color = '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6);
         work.style.backgroundColor = color;
         panel.append(work);
     }
 
-    // Comments slider
+    // Comments slider 
     const wrapper = document.querySelector(".comments__slider-wrapper"),
-          inner = document.querySelector(".comments__slider-inner"),
-          width = window.getComputedStyle(wrapper).width,
-          slides = document.querySelectorAll(".comments__slider-item"),
-          faces = document.querySelectorAll(".comments__responder"),
-          facesPanel = document.querySelector(".comments__responder-panel");
+        inner = document.querySelector(".comments__slider-inner"),
+        width = window.getComputedStyle(wrapper).width,
+        slides = document.querySelectorAll(".comments__slider-item"),
+        faces = document.querySelectorAll(".comments__responder"),
+        facesPanel = document.querySelector(".comments__responder-panel");
 
     inner.style.width = 100 * slides.length + '%';
     inner.style.display = 'flex';
     inner.style.transition = '0.8s all';
     wrapper.style.overflow = 'hidden';
-    
+    let offset = 2 * parseInt(width, 10);
+    inner.style.transform = `translateX(-${offset}px)`;
+
     slides.forEach(slide => {
         slide.style.width = width;
     });
 
-    facesPanel.addEventListener("click", function(e) {
+    facesPanel.addEventListener("click", function (e) {
         let target = e.target;
         if (target.hasAttribute("alt")) {
             faces.forEach(item => {
@@ -102,7 +104,7 @@ document.addEventListener('DOMContentLoaded', () => {
             });
             target.parentElement.classList.add("comments__responder_active");
         }
-        let offset = (target.parentElement.dataset.slideTo - 1) * parseInt(width, 10);
+        offset = (target.parentElement.dataset.slideTo - 1) * parseInt(width, 10);
         inner.style.transform = `translateX(-${offset}px)`;
     });
 }); 
